a22e58b694cf1c20c3bd002ca24da792e78a978f
EdithMR <edithmandujano001@gmail.com>
2025-09-17 18:38:33 -0600

    Se agregan signals y se cuentan las tareas y se da la opción para selecciònar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { NgFor } from "@angular/common";
 
+import { Task } from '../../models/task.model';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,22 +11,55 @@ import { NgFor } from "@angular/common";
   imports: [ NgFor ]
 })
 export class HomeComponent {
-  tasks = signal([
-      'Tarea1',
-      'Tarea2',
-      'Tarea3',
-      'Tarea4'
+  tasks = signal<Task[]>([
+      {
+        id: Date.now(),
+        title:'Crear proyecto',
+        completed: true
+      },
+      {
+        id: Date.now(),
+        title:'Editar proyecto',
+        completed: false
+      },
+      {
+        id: Date.now(),
+        title:'Eliminar proyecto',
+        completed: false
+      }
   ]);
 
   changeHandlerInput(event:Event){
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask)
+  }
+
+  addTask(title: string){
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed:false,
+    };
     //para  agregar nuevo valor  al final de la lista
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
-
   deleteTask(index: Number){
     //usamos filter para hacer un array sin el elemanto con el index enviado
     this.tasks.update((tasks)=>tasks.filter((task, position)=> position != index));
+  }
+
+  updateTask(index:Number){
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return{
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    });
   }
 }
