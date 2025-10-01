0c737c797d97f4d0f45f24d24f5fc5fcc893a932
EdithMR <edithmandujano001@gmail.com>
2025-09-30 18:28:14 -0600

    se agrega edición de tareas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -38,8 +38,11 @@ export class HomeComponent {
   })
   changeHandlerInput(){
     if(this.newTaskCtrl.valid){
-      const value= this.newTaskCtrl.value;
-      this.addTask(value);
+      const value= this.newTaskCtrl.value.trim();
+      if (value !== ''){
+        this.addTask(value);
+      }
+      
       this.newTaskCtrl.setValue('');
     }
     
@@ -69,6 +72,38 @@ export class HomeComponent {
           }
         }
         return task;
+      })
+    });
+  }
+
+  updateTaskEditingMode(index: number){
+    this.tasks.update(prevState =>{
+      return prevState.map((task, position)=>{
+        if (position === index){
+          return{
+            ...task,
+            editing: true
+          }
+        }
+        return {
+          ...task,
+          editing:false
+        }
+      })
+    });
+  }
+  updateTaskText(index:number, event: Event){
+    const input =event.target as HTMLInputElement;
+    this.tasks.update(prevState =>{
+      return prevState.map((task, position)=>{
+        if (position === index){
+          return{
+            ...task,
+            title: input.value,
+            editing:false
+          }
+        }
+        return task
       })
     });
   }
--- src/app/pages/lab/lab.component.ts
+++ src/app/pages/lab/lab.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
@@ -34,6 +34,16 @@ export class LabComponent {
   });
 
   colorCtrl = new FormControl();
+  widthCtrl = new FormControl(50, {
+    nonNullable:true,
+  });
+  nameCtrl = new FormControl('Lucia',{
+    nonNullable:true,
+    validators:[
+      Validators.required,
+      Validators.minLength(3)
+    ]
+  });
 
   constructor(){
     this.colorCtrl.valueChanges.subscribe(value =>{
